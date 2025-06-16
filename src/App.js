1eca1cf4b175eb52e64476dc123b3fc764830e73
Natnicha Kittidilok <natnicha.kii@student.mahidol.edu>
2025-06-16 21:46:29 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserHome from "./pages/UserHome";
+import EvaluationForm from "./pages/EvaluationForm";
+import AdminHome from "./pages/AdminHome";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserHome />} />
+        <Route path="/evaluate/:id" element={<EvaluationForm />} />
+        <Route path="/admin" element={<AdminHome />} />  {/* <-- เพิ่มบรรทัดนี้ */}
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
