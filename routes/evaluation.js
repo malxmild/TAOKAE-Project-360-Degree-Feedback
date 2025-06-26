6bc727ffa10126b19e02a4cfdf180293a59b1a64
Natnicha Kittidilok <168412576+malxmild@users.noreply.github.com>
2025-06-26 14:46:45 +0700

    Delete routes directory

--- routes/evaluation.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authenticate = require('../middleware/authenticate');
-const { submitEvaluation } = require('../controllers/evaluationController');
-
-//การส่งแบบประเมิน
-router.post('/submit', authenticate, submitEvaluation);
-
-module.exports = router;
